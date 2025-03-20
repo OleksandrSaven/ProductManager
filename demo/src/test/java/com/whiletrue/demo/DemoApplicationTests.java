7996895eb4d3f95a1859cf72cb480c5924addc2b
Oleksandr Savenets <savenvoin@gmail.com>
2025-03-20 15:11:11 +0200

    initial commit

---
+++ demo/src/test/java/com/whiletrue/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiletrue.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
